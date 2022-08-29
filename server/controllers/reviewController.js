2044d6b26715741ecedba607af2072a33943e9e7
RonenTal <talker.ronen@gmail.com>
2022-08-29 22:09:13 +0300

    updated

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -2,6 +2,5 @@ const mongoose = require("mongoose");
 
 
 const Product = require("../models/reviewModel");
-// const product = require("../models/productModel");
 const {sendRes} = require("../helpers/sendRes"); 
 const { makeFilterObject } = require("../helpers/makeFilterObject");

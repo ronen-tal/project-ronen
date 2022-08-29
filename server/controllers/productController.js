d3f84f31dbeb46d3b8b99f720e6f3265a3a6652c
RonenTal <talker.ronen@gmail.com>
2022-08-29 21:45:43 +0300

    updated product control and route

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose");
+
+
 const Product = require("../models/productModel");
 // const product = require("../models/productModel");
 const {sendRes} = require("../helpers/sendRes"); 
@@ -34,11 +37,71 @@ const filterObject = makeFilterObject(req.query);
 
     module.exports.deleteProduct = async (req,res) =>{
         const {id} = req.body;
-        const del = makeFilterObject(req.query);
+        const del = makeFilterObject(id);
         try{
             const product = await Product.deleteOne(del);
             sendRes(res,product,200);
         }catch(err){
             sendRes(res,err,400,true);
         }}
+
+module.exports.getProductById = async (req,res) => {
+    const {id} = req.params;
+    try{
+        const product = await Product.findById(id);
+        if(!product) throw new Error ("id dose not exsit")
+        sendRes(res,product,200);
+    }catch(err){
+        sendRes(res,err,400,true);
+    }
+}
+
+
+module.exports.deleteProductById = async (req,res) => {
+    const {id} = req.params;
+    try{
+         await Product.deleteOne({_id: id});
+        sendRes(res,{},204); // {} empty object allow as to delete by id witout using nall
+    }catch(err){
+        sendRes(res,err,400,true);
+    }
+};
+
+module.exports.checkValidId = (req,res,next)=>{   //middelware for checking id inside get and delete route
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+        const error = {message:"invalid id"};
+        sendRes(res,error,400,true);
+        return
+    }
+    next()
+};
+
+module.exports.updateProduct =  async (req,res) => {
+    const {id} = req.params;
+    // delete body["id"]
+    try{
+        const product =await Product.findByIdAndUpdate(id,req.body,{
+        new:true,
+        runValidators:true,
+    });
+    // const products = await Product.find({category:"Jewellery"}) // if we want to change to all category the price or somthing else 
+    // products.forEach(pr =>{
+    //     pr.price = 1;
+    //     pr= pr.save({
+    //         runValidators:true,
+    //     })
+    // })
+        sendRes(res,product,200);
+    }catch(err){
+        sendRes(res,err,400,true);
+    }
+}; 
+
+
+
+
+
+
+
+
 
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,6 +4,15 @@ const productController = require("../controllers/productController")
 
 const router = express.Router();
 
-router.route("/").get(productController.getAllProducts).post(productController.createNewProduct).delete(productController.deleteProduct)
+router.route("/")
+.get(productController.getAllProducts)
+.post(productController.createNewProduct)
+.delete(productController.deleteProduct)
 
-module.exports = router
+router.route("/:id")
+.all(productController.checkValidId) // all is saved word in js 
+.get(productController.getProductById)
+.delete(productController.deleteProductById)
+.put(productController.updateProduct)
+
+module.exports = router;
